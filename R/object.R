4bc65f63b36f12ebd80bd1fd79eea166a6d7a8e1
alex hayes <alexpghayes@gmail.com>
2020-09-23 15:14:33 -0600

    MVP implementation

--- R/object.R
+++ R/object.R
@@ -11,10 +11,6 @@
 #' @slot U Matrix.
 #' @slot V Matrix.
 #'
-#' @return
-#' @export
-#'
-#' @examples
 setClass(
   Class = "sparseLRMatrix",
   # contains = "Matrix",
@@ -30,11 +26,23 @@ setClass(
   )
 )
 
-# the helper
+#' Title
+#'
+#' @param sparse TODO
+#' @param U TODO
+#' @param V TODO
+#'
+#' @return
+#' @export
+#' @importFrom methods new
+#'
+#' @examples
+#'
+#' # TODO
 sparseLRMatrix <- function(sparse, U, V) {
-  new(
+  methods::new(
     Class = "sparseLRMatrix",
-    sparse = A,
+    sparse = sparse,
     U = Matrix(U),
     V = Matrix(V)
   )
@@ -55,31 +63,47 @@ setValidity("sparseLRMatrix", function(object) {
 setMethod("dim", "sparseLRMatrix", function(x) dim(x@sparse))
 
 Ax <- function(x, A) {
-  A@sparse %*% x + A@U %*% crossprod(A@V, x)
+  out <- A@sparse %*% x + A@U %*% Matrix::crossprod(A@V, x)
+  drop(out)
 }
 
 Atx <- function(x, A) {
-  crossprod(A@sparse, x) + A@V %*% crossprod(A@U, x)
+  out <- crossprod(A@sparse, x) + A@V %*% Matrix::crossprod(A@U, x)
+  drop(out)
 }
 
+#' @export
+#' @importFrom RSpectra svds
+svds <- RSpectra::svds
 
-#' @inherit RSpectra::svds title params return
+#' Truncated singular value decomposition of a matrix
+#'
+#' A thin wrapper around [RSpectra::svds()], please see more detailed
+#' documentation there. In particular, this function leverages the
+#' function interface.
+#'
+#' @param A Matrix to decompose.
+#' @param k Number of singular values to estimate.
+#' @param nu Number of left singular vectors to estimate.
+#' @param nv Number of right singular vectors to estimate.
+#' @param opts Passed to [RSpectra::svds()].
+#' @param ... Passed to [RSpectra::svds()].
+#'
 #' @method svds sparseLRMatrix
 #' @export
 #'
 #' @examples
 #'
-#' library(RSpectra)
-#'
 #' set.seed(528491)
 #'
 #' n <- 50
+#' m <- 40
 #' k <- 3
 #'
-#' A <- rsparsematrix(n, n, 0.1)
+#' A <- rsparsematrix(n, m, 0.1)
 #'
 #' U <- Matrix(rnorm(n * k), nrow = n, ncol = k)
-#' V <- Matrix(rnorm(n * k), nrow = n, ncol = k)
+#' V <- Matrix(rnorm(m * k), nrow = m, ncol = k)
 #'
 #' X <- sparseLRMatrix(sparse = A, U = U, V = V)
 #'
@@ -94,7 +118,8 @@ svds.sparseLRMatrix <- function(A, k, nu = k, nv = k, opts = list(), ...) {
     opts = opts,
     Atrans = Atx,
     dim = dim(A),
-    args = A
+    args = A,
+    ...
   )
 }
 
